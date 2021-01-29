3166274f9d5fdec7d59b57e6773736739c106ebb
droid003best <54209012+droid003best@users.noreply.github.com>
2021-01-29 15:46:39 +0100

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -1,2 +1,2 @@
-Get-AppxPackage -allusers Microsoft.549981C3F5F10 | Remove-AppxPackage
-pause
+powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+schtasks /create
