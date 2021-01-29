dc18e5db2af79860e79544c20793608e71c08071
droid003best <54209012+droid003best@users.noreply.github.com>
2021-01-29 18:49:03 +0100

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -1,5 +1,5 @@
-powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/wSetup/main/choco.bat', '%Temp%\wSetup\install.bat') }"
 SCHTASKS /CREATE /SC onlogon /TN "choco" /TR "%temp%\install.bat" /rl HIGHEST
 SCHTASKS /RUN /TN "choco"
 SCHTASKS /DELETE /TN "choco" /f
