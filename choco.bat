a178ea97ff5245127c79d6158119600568fcc38d
droid003best <54209012+droid003best@users.noreply.github.com>
2021-01-29 18:50:10 +0100

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -1,5 +1,4 @@
-
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/wSetup/main/choco.bat', '%Temp%\wSetup\install.bat') }"
-SCHTASKS /CREATE /SC onlogon /TN "choco" /TR "%temp%\install.bat" /rl HIGHEST
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/wSetup/main/install.bat', '%Temp%\wSetup\install.bat') }"
+SCHTASKS /CREATE /SC onlogon /TN "choco" /TR "%temp%\wSetup\install.bat" /rl HIGHEST
 SCHTASKS /RUN /TN "choco"
 SCHTASKS /DELETE /TN "choco" /f
