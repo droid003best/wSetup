462b63eb6718dbe3487bff1e19a591e2128f2650
droid003best <54209012+droid003best@users.noreply.github.com>
2021-02-05 19:44:07 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-set-executionpolicy remotesigned | iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/droid003best/wSetup/main/main.ps1'))
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/droid003best/wSetup/main/main.ps1'))
