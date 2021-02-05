495774d423d80b6826261d6aa8477dfb4b52916c
droid003best <54209012+droid003best@users.noreply.github.com>
2021-02-05 19:43:37 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,1 @@
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/wSetup/main/wSetup.bat', '%Temp%\wSetup.bat') }" & %temp%\wSetup.bat
-
+set-executionpolicy remotesigned | iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/droid003best/wSetup/main/main.ps1'))
