030cbf8b3e2d5856bf8cb0fa7f9428a2ce6e7357
droid003best <54209012+droid003best@users.noreply.github.com>
2021-02-13 11:21:03 +0100

    Update wSetup.ps1

--- wSetup.ps1
+++ wSetup.ps1
@@ -1,9 +1,3 @@
-# Relaunch the script with administrator privileges
-	If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-		Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -WorkingDirectory $pwd -Verb RunAs
-		Exit
-	}
-	
 #Chocolatey
 	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/droid003best/wSetup/main/wSetup.ps1" -OutFile c:\choco.ps1
 	start powershell {c:\choco.ps1}
