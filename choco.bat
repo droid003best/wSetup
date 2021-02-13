39820d88311296eedc57605f0933550e9b5fa2e3
droid003best <54209012+droid003best@users.noreply.github.com>
2021-02-13 11:14:07 +0100

    Update and rename choco.bat to choco.ps1

--- choco.bat
+++
@@ -1,5 +0,0 @@
-powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/droid003best/wSetup/main/install.bat', '%Temp%\wSetup\install.bat') }"
-SCHTASKS /CREATE /SC onlogon /TN "choco" /TR "%temp%\wSetup\install.bat" /rl HIGHEST
-SCHTASKS /RUN /TN "choco"
-SCHTASKS /DELETE /TN "choco" /f
