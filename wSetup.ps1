293f5860f5e8aa24be488bfdf9a729c2019d7b12
droid003best <54209012+droid003best@users.noreply.github.com>
2021-02-13 11:18:50 +0100

    Update wSetup.ps1

--- wSetup.ps1
+++ wSetup.ps1
@@ -1,4 +1,4 @@
-# Relaunch the script with administrator privilege
+# Relaunch the script with administrator privileges
 	If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
 		Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -WorkingDirectory $pwd -Verb RunAs
 		Exit
