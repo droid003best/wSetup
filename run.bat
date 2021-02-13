6af30393f8cbeebab3e3dd016a34283caa4de0a7
droid003best <54209012+droid003best@users.noreply.github.com>
2021-02-13 10:53:37 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/droid003best/wSetup/main/main.ps1'))
+
+set-executionpolicy remotesigned | Start-BitsTransfer -Source "https://raw.githubusercontent.com/droid003best/wSetup/main/wSetup.ps1" -Destination "c:\wSetup.ps1" | c:\wSetup.ps1
